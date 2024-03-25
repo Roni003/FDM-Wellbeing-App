73829087e2d0421ff78c3dd49d0de20a81722691
Roni003 <ronikazmirci003@gmail.com>
2024-03-25 14:00:50 +0000

    Made back button component (needs style fix)

--- app/tracker/sleep.tsx
+++ app/tracker/sleep.tsx
@@ -3,9 +3,12 @@ import { StyleSheet } from "react-native";
 import { Text, View } from "@/components/Themed";
 import { Link } from "expo-router";
 
+import BackButton from "@/components/BackButton";
+
 export default function SleepScreen() {
   return (
     <View style={styles.container}>
+      <BackButton destination="/user/(tabs)/" />
       <Text>sleep tracker screen</Text>
       <Link href="/user/(tabs)">
         <Text>Link back to user tabs -for convenience in development</Text>
