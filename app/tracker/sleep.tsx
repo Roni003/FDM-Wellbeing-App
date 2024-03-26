5cfef074a3c58191bce057521781987a33bb4bb3
Muhammed Fahim Miah <ec22717@qmul.ac.uk>
2024-03-26 15:24:53 +0000

    Implemented sleep tracker feature

--- app/tracker/sleep.tsx
+++ app/tracker/sleep.tsx
@@ -1,21 +1,53 @@
-import { StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, TextInput, TouchableHighlight } from "react-native";
 
 import { Text, View } from "@/components/Themed";
-import { Link } from "expo-router";
-
 import BackButton from "@/components/BackButton";
 
 export default function SleepScreen() {
+  const [sleepDuration, setSleepDuration] = useState("");
+  const [trackedSleep, setTrackedSleep] = useState<number | null>(null);
+
+  const handleTrackSleep = () => {
+    const duration = parseFloat(sleepDuration);
+    if (!isNaN(duration)) {
+      setTrackedSleep(duration);
+      setSleepDuration("");
+    } else {
+      console.log("Invalid input. Please enter a valid number.");
+    }
+  };
+
+  const handleSleepDurationChange = (text: string) => {
+    const numericInput = text.replace(/[^0-9]/g, "");
+    setSleepDuration(numericInput);
+  };
+
   return (
     <View style={styles.container}>
       <BackButton destination="/user/(tabs)/" />
-      <Text>sleep tracker screen</Text>
-      <Link href="/user/(tabs)">
-        <Text>Link back to user tabs -for convenience in development</Text>
-      </Link>
+      <Text style={styles.title}>Sleep Tracker</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Enter sleep duration"
+          style={styles.input}
+          onChangeText={handleSleepDurationChange}
+          value={sleepDuration}
+          keyboardType="numeric"
+        />
+      </View>
+      <TouchableHighlight style={styles.button} onPress={handleTrackSleep}>
+        <Text style={styles.buttonText}>Track Sleep</Text>
+      </TouchableHighlight>
+      {trackedSleep !== null && (
+        <Text style={styles.trackedSleep}>
+          Tracked Sleep Duration: {trackedSleep} hours
+        </Text>
+      )}
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -27,9 +59,31 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "bold",
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+  inputContainer: {
+    backgroundColor: "white",
+    width: "100%",
+    borderColor: "grey",
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 4,
+    marginVertical: 5,
+  },
+  input: {},
+  button: {
+    backgroundColor: "#3ec6ff",
+    width: "50%",
+    borderColor: "grey",
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 13,
+    marginVertical: 5,
+    alignItems: "center",
+  },
+  buttonText: {
+    fontWeight: "bold",
+  },
+  trackedSleep: {
+    marginTop: 10,
   },
 });
