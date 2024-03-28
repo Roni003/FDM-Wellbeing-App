2c5c7e236feaf43a5e953fd9a2759e0026e423dd
Roni K <61784529+Roni003@users.noreply.github.com>
2024-03-28 15:15:13 +0000

    added padding to container and title

--- app/user/(tabs)/index.tsx
+++ app/user/(tabs)/index.tsx
@@ -5,16 +5,14 @@ import { Link } from "expo-router";
 import Tracker from "@/components/Tracker";
 
 export default function TabOneScreen() {
-
   return (
-    <View style ={{flex:1}}>
-
-      <View style ={{marginTop:'10%', flex:1}}>
-
-        <Text style={{fontSize:25, marginLeft:'1%'}}>Trackers</Text>
+    <View style={{ flex: 1, padding: 10 }}>
+      <View style={{ marginTop: "10%", flex: 1 }}>
+        <Text style={{ fontSize: 25, marginLeft: "1%", paddingBottom: 5 }}>
+          Trackers
+        </Text>
 
         <View style={styles.tracker_grid}>
-
           <View style={styles.tracker_row}>
             <Tracker
               head="Todays steps"
@@ -40,85 +38,91 @@ export default function TabOneScreen() {
               link="/tracker/sleep"
             />
           </View>
-
         </View>
-
       </View>
 
-      
       <View style={styles.forum}>
-        <Text style={{fontSize:25, marginLeft:'1%'}}>Recent Forum Posts</Text>
-        <ScrollView style={{maxHeight:140, borderRadius: 10, borderWidth: 0, borderColor: 'white'}}>
-        <View style={styles.notif_container}>
-          <Text>Forum Post Title </Text>
-          <Text>By: user1 @ 00:00</Text>
-        </View>
-        <View style={styles.notif_container}>
+        <Text style={{ fontSize: 25, marginLeft: "1%" }}>
+          Recent Forum Posts
+        </Text>
+        <ScrollView
+          style={{
+            maxHeight: 140,
+            borderRadius: 10,
+            borderWidth: 0,
+            borderColor: "white",
+          }}
+        >
+          <View style={styles.notif_container}>
             <Text>Forum Post Title </Text>
             <Text>By: user1 @ 00:00</Text>
-        </View>
-        <View style={styles.notif_container}>
+          </View>
+          <View style={styles.notif_container}>
             <Text>Forum Post Title </Text>
             <Text>By: user1 @ 00:00</Text>
-        </View>
-        <View style={styles.notif_container}>
+          </View>
+          <View style={styles.notif_container}>
             <Text>Forum Post Title </Text>
             <Text>By: user1 @ 00:00</Text>
-        </View>
+          </View>
+          <View style={styles.notif_container}>
+            <Text>Forum Post Title </Text>
+            <Text>By: user1 @ 00:00</Text>
+          </View>
         </ScrollView>
-        <Link href="/user/(tabs)/forum" style={styles.link}> 
-            <Text>Go to forum</Text>
+        <Link href="/user/(tabs)/forum" style={styles.link}>
+          <Text>Go to forum</Text>
         </Link>
       </View>
-    
-      <Link href="/" style={{marginTop:10}}>
-            <Text>Return to login</Text>
+
+      <Link href="/" style={{ marginTop: 10 }}>
+        <Text>Return to login</Text>
       </Link>
     </View>
   );
 }
 
-const screenHeight = Dimensions.get('window').height;
+const screenHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'rgba(200, 200, 200, 0.1)',
+    backgroundColor: "rgba(200, 200, 200, 0.1)",
   },
 
-  forum : {
+  forum: {
     flex: 1,
     maxHeight: screenHeight * 0.3,
   },
 
   tracker_grid: {
     flex: 1,
-    flexDirection: 'column',
+    flexDirection: "column",
     height: screenHeight * 0.5,
   },
 
   tracker_row: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: "row",
   },
 
   link: {
-    backgroundColor: 'rgba(100, 160, 255, 0.5)',
+    backgroundColor: "rgba(100, 160, 255, 0.5)",
     padding: 5,
     borderRadius: 10,
     borderWidth: 0.5,
-    borderColor: 'rgba(250, 250, 250, 0.2)',
-    overflow: 'hidden',
-    maxWidth: '30%',
-    margin: '2%',
-    textAlign: 'center'
+    borderColor: "rgba(250, 250, 250, 0.2)",
+    overflow: "hidden",
+    maxWidth: "30%",
+    margin: "2%",
+    textAlign: "center",
   },
 
   notif_container: {
     margin: 7,
     borderRadius: 10,
-    borderColor: 'rgba(250, 250, 250, 0.2)',
+    borderColor: "rgba(250, 250, 250, 0.2)",
     borderWidth: 0.5,
     padding: 7,
-    backgroundColor: 'rgba(100, 160, 255, 0.5)',
+    backgroundColor: "rgba(100, 160, 255, 0.5)",
   },
 });
