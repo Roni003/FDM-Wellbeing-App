1c63f0ee5f1ba431830d187ce8d93e0b6f053dc8
ec22535 <ec22535@qmul.ac.uk>
2024-04-03 13:14:40 +0100

    paths

--- app/tracker/meditation.tsx
+++ app/tracker/meditation.tsx
@@ -19,21 +19,21 @@ const MeditationApp = () => {
 
   const meditationSessions = [
     { id: 1, level: 'Beginner', name: 'Quick Meditation', duration: 5 * 60, audioPath: require('./../../assets/audio/5min.mp3'), image: require('./../../assets/images/calm.jpg') },
-    { id: 2, level: 'Beginner', name: 'Let Go Of Stress', duration: 10 * 60, audioPath: require('./../../../assets/audio/10min.mp3'), image: require('./../../../assets/images/letGo.jpeg') },
-    { id: 3, level: 'Intermediate', name: 'Train Your Mind', duration: 20 * 60, audioPath: require('./../../../assets/audio/20min.mp3'), image: require('./../../../assets/images/trainMind.jpg') },
-    {id: 14, level: 'Intermidiate', name: 'Daily Calm', duration: 30 * 60, audioPath: require('./../../../assets/audio/30min.mp3'), image: require('./../../../assets/images/nature.jpg') },
-    { id: 4, level: 'Advanced', name: 'Deep Meditation', duration: 45 * 60, audioPath: require('./../../../assets/audio/45min.mp3'), image: require('./../../../assets/images/deep.jpg') },
+    { id: 2, level: 'Beginner', name: 'Let Go Of Stress', duration: 10 * 60, audioPath: require('./../..//assets/audio/10min.mp3'), image: require('./../..//assets/images/letGo.jpeg') },
+    { id: 3, level: 'Intermediate', name: 'Train Your Mind', duration: 20 * 60, audioPath: require('./../..//assets/audio/20min.mp3'), image: require('./..//../assets/images/trainMind.jpg') },
+    {id: 14, level: 'Intermidiate', name: 'Daily Calm', duration: 30 * 60, audioPath: require('./../..//assets/audio/30min.mp3'), image: require('./../..//assets/images/nature.jpg') },
+    { id: 4, level: 'Advanced', name: 'Deep Meditation', duration: 45 * 60, audioPath: require('./../..//assets/audio/45min.mp3'), image: require('./..//../assets/images/deep.jpg') },
   ];
 
   const intro = [
-    { id: 5, level: 'Beginner', name: 'How-To Meditate', videoSource: require('./../../../assets/video/HowTo.mp4'), image: 'https://i.pinimg.com/originals/5c/43/ee/5c43ee2ccc1076dfbad0281c948406be.png' },
+    { id: 5, level: 'Beginner', name: 'How-To Meditate', videoSource: require('./../..//assets/video/HowTo.mp4'), image: 'https://i.pinimg.com/originals/5c/43/ee/5c43ee2ccc1076dfbad0281c948406be.png' },
   ];
 
   const exercises = [
-    { id: 6, level: 'Beginner', name: 'Deep Breathing', videoSource: require('./../../../assets/video/deepBreathing.mp4'), image: 'https://post.healthline.com/wp-content/uploads/2022/11/400x400_Breathing_Techniques_For_Stress_Relief_and_More_Deep_Breathing.gif' },
-    { id: 7, level: 'Intermediate', name: 'Square Breathing', videoSource: require('./../../../assets/video/boxBreathing.mp4'), image: 'https://post.healthline.com/wp-content/uploads/2022/11/400x400_Breathing_Techniques_For_Stress_Relief_and_More_Equal_Breathing.gif' },
-    { id: 8, level: 'Intermediate', name: 'Lions Breathing', videoSource: require('./../../../assets/video/lionsBreathing.mp4'), image: 'https://post.healthline.com/wp-content/uploads/2022/11/400x400_Practicing_Lions_Breath_Lions_Breath.gif' },
-    { id: 9, level: 'Advanced', name: 'Alternate Nostril', videoSource: require('./../../../assets/video/nostrilBreathing.mp4'), image: 'https://post.healthline.com/wp-content/uploads/2022/11/400x400_Breathing_Techniques_For_Stress_Relief_and_More_Alternate_Nostril_Breathing.gif' },
+    { id: 6, level: 'Beginner', name: 'Deep Breathing', videoSource: require('./../../assets/video/deepBreathing.mp4'), image: 'https://post.healthline.com/wp-content/uploads/2022/11/400x400_Breathing_Techniques_For_Stress_Relief_and_More_Deep_Breathing.gif' },
+    { id: 7, level: 'Intermediate', name: 'Square Breathing', videoSource: require('./../../assets/video/boxBreathing.mp4'), image: 'https://post.healthline.com/wp-content/uploads/2022/11/400x400_Breathing_Techniques_For_Stress_Relief_and_More_Equal_Breathing.gif' },
+    { id: 8, level: 'Intermediate', name: 'Lions Breathing', videoSource: require('./../../assets/video/lionsBreathing.mp4'), image: 'https://post.healthline.com/wp-content/uploads/2022/11/400x400_Practicing_Lions_Breath_Lions_Breath.gif' },
+    { id: 9, level: 'Advanced', name: 'Alternate Nostril', videoSource: require('./../../assets/video/nostrilBreathing.mp4'), image: 'https://post.healthline.com/wp-content/uploads/2022/11/400x400_Breathing_Techniques_For_Stress_Relief_and_More_Alternate_Nostril_Breathing.gif' },
   ];
 
   const extras = [
