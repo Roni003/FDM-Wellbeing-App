1f89ecf9b8ecf0e6c4afec2393f2ef5b98c3f8e0
Suraj Shah <s.shah@se22.qmul.ac.uk>
2024-04-03 19:28:07 +0100

    improved styling for creating post page

--- app/forumpost/createPost.tsx
+++ app/forumpost/createPost.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Button, TextInput, Alert } from "react-native";
+import { StyleSheet, Button, TextInput, Alert, useColorScheme, TouchableOpacity } from "react-native";
 import { Formik, Field, Form } from "formik";
 import { supabase } from "@/lib/Supabase";
 import React, { useState } from "react";
@@ -7,14 +7,94 @@ import { Text, View } from "@/components/Themed";
 import { Link, router } from "expo-router";
 import BackButton from "@/components/BackButton";
 import { err } from "react-native-svg";
+import Colors from "@/lib/Colors";
+import { globalStyles } from "@/lib/Styles";
 
 export default function CreatePostForm() {
   const [titleError, setTitleError] = useState(false);
   const [bodyError, setBodyError] = useState(false);
   const [errorText, setErrorText] = useState("");
 
+  const colorScheme = useColorScheme();
+  const textColor =
+    colorScheme === "light" ? Colors.light.text : Colors.dark.text;
+
+  const styles = StyleSheet.create({
+    inputReset: {
+      borderColor: colorScheme === "light"
+      ? Colors.light.text
+      : Colors.dark.text,
+    },
+  
+    inputError: {
+      borderColor: "red",
+    },
+  
+    label: {},
+  
+    titleInput: {
+      backgroundColor:colorScheme === "light"
+      ? Colors.light.cardBackground
+      : Colors.dark.cardBackground,
+      color: textColor,
+      borderWidth: 1,
+      borderColor: "#ddd",
+      padding: 10,
+      margin: 10,
+      borderRadius: 6,
+      fontSize: 18,
+      width: '95%',
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  
+    bodyInput: {
+      backgroundColor:colorScheme === "light"
+      ? Colors.light.cardBackground
+      : Colors.dark.cardBackground,
+      color: textColor,
+      borderWidth: 1,
+      padding: 10,
+      margin: 10,
+      borderRadius: 6,
+      fontSize: 18,
+      width: '95%',
+      height: '70%',
+    },
+  
+    title: {
+      fontSize: 20,
+      fontWeight: "bold",
+    },
+    separator: {
+      marginVertical: 30,
+      height: 1,
+      width: "80%",
+    },
+    button: {
+      height: 50,
+      width: '70%',
+      marginVertical: 10,
+      borderRadius: 8,
+      backgroundColor: colorScheme === "light"
+      ? Colors.light.cardBackground
+      : Colors.dark.cardBackground,
+      padding: 15,
+      alignItems: 'center',
+      alignSelf: 'center',
+    },
+    textStyle: {
+      fontSize: 16,
+      color: textColor,
+    },
+    placeholderText: {
+      fontSize: 16,
+      color: textColor,
+    }
+  });
+
   return (
-    <View style={styles.container}>
+    <View style={globalStyles.container}>
       <BackButton destination={"/"} name={"home page"} />
       <Formik
         initialValues={{ title: "", body: "" }}
@@ -69,6 +149,7 @@ export default function CreatePostForm() {
                 styles.inputReset,
                 titleError && styles.inputError,
               ]}
+              placeholderTextColor={styles.placeholderText.color}
             />
 
             <TextInput
@@ -81,71 +162,16 @@ export default function CreatePostForm() {
                 styles.inputReset,
                 bodyError && styles.inputError,
               ]}
+              placeholderTextColor={styles.placeholderText.color}
             />
 
             <Text style={styles.errorText}>{errorText}</Text>
-            <Button title="submit" onPress={props.handleSubmit} />
+            <TouchableOpacity style={styles.button} onPress={props.handleSubmit}>
+              <Text style={styles.textStyle}>Submit</Text>
+            </TouchableOpacity>
           </View>
         )}
       </Formik>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  inputReset: {
-    borderColor: "white",
-  },
-
-  inputError: {
-    borderColor: "red",
-  },
-
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  label: {},
-  errorText: {
-    color: "red",
-    fontWeight: "600",
-    fontSize: 16,
-  },
-
-  titleInput: {
-    color: "white",
-    borderWidth: 1,
-    borderColor: "#ddd",
-    padding: 10,
-    margin: 10,
-    borderRadius: 6,
-    fontSize: 18,
-    width: 150,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  bodyInput: {
-    color: "white",
-    borderWidth: 1,
-    borderColor: "#ddd",
-    padding: 10,
-    margin: 10,
-    borderRadius: 6,
-    fontSize: 18,
-    width: 150,
-    height: 250,
-  },
-
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
