38017c09e83828b9f360ff9513ae5dd9e381e0d9
ec22535 <ec22535@qmul.ac.uk>
2024-04-03 13:22:36 +0100

    paths

--- app/tracker/meditation.tsx
+++ app/tracker/meditation.tsx
@@ -37,10 +37,10 @@ const MeditationApp = () => {
   ];
 
   const extras = [
-    { id: 10, level: 'Intermediate', name: 'Movement', videoSource: require('./../../../assets/video/movement.mp4'), image: 'https://imageio.forbes.com/blogs-images/alicegwalton/files/2015/02/0728_deep-brain-stimulation_650x455.jpg?height=455&width=650&fit=bounds' },
-    { id: 11, level: 'Intermediate', name: 'Mantra', videoSource: require('./../../../assets/video/mantra.mp4'), image: 'https://images.squarespace-cdn.com/content/v1/5b2a8a5a45776ef37acb6ad6/1571914374300-0PR69AZRU3EXF5HF0635/image-asset.jpeg?format=1500w' },
-    { id: 12, level: 'Intermediate', name: 'Visualization', videoSource: require('./../../../assets/video/visualisation.mp4'), image: 'https://img.freepik.com/free-photo/3d-abstract-flowing-geometric-shapes_1048-11947.jpg?w=996&t=st=1711854821~exp=1711855421~hmac=3b3d575344aa2be52edd4d0861258d3a7fba0f2cbf92079a82f6ab47bac348c2' },
-    { id: 13, level: 'Advanced', name: 'Body Scan', videoSource: require('./../../../assets/video/bodyScan.mp4'), image: 'https://media.istockphoto.com/id/1330215408/photo/visualized-3d-models-of-the-female-human-body-as-well-as-the-human-skeleton-in-x-rays-using.webp?s=2048x2048&w=is&k=20&c=re2rCUEkqBG5d5tb2tWeYiJSPwjnOs6DXsRpk2tbeqQ=' },
+    { id: 10, level: 'Intermediate', name: 'Movement', videoSource: require('./../../assets/video/movement.mp4'), image: 'https://imageio.forbes.com/blogs-images/alicegwalton/files/2015/02/0728_deep-brain-stimulation_650x455.jpg?height=455&width=650&fit=bounds' },
+    { id: 11, level: 'Intermediate', name: 'Mantra', videoSource: require('./../../assets/video/mantra.mp4'), image: 'https://images.squarespace-cdn.com/content/v1/5b2a8a5a45776ef37acb6ad6/1571914374300-0PR69AZRU3EXF5HF0635/image-asset.jpeg?format=1500w' },
+    { id: 12, level: 'Intermediate', name: 'Visualization', videoSource: require('./../../assets/video/visualisation.mp4'), image: 'https://img.freepik.com/free-photo/3d-abstract-flowing-geometric-shapes_1048-11947.jpg?w=996&t=st=1711854821~exp=1711855421~hmac=3b3d575344aa2be52edd4d0861258d3a7fba0f2cbf92079a82f6ab47bac348c2' },
+    { id: 13, level: 'Advanced', name: 'Body Scan', videoSource: require('./../../assets/video/bodyScan.mp4'), image: 'https://media.istockphoto.com/id/1330215408/photo/visualized-3d-models-of-the-female-human-body-as-well-as-the-human-skeleton-in-x-rays-using.webp?s=2048x2048&w=is&k=20&c=re2rCUEkqBG5d5tb2tWeYiJSPwjnOs6DXsRpk2tbeqQ=' },
   ];
 
   const dailyGoal = 10;
