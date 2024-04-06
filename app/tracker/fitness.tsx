6abb6ef6e8ef3ff2aaa7f41fbaa14f53e6d68be0
Karishan <ec22899@qmul.ac.uk>
2024-04-06 18:25:14 +0100

    stores fitness details depending on whether they have already sotred today or not. Updates if exists already or inserts if not

--- app/tracker/fitness.tsx
+++ app/tracker/fitness.tsx
@@ -1,17 +1,19 @@
 import { StyleSheet, TextInput, TouchableOpacity, ScrollView, KeyboardAvoidingView, Platform, SafeAreaView, StatusBar, useColorScheme } from "react-native";
 import { Text, View } from "@/components/Themed";
 import { Link } from "expo-router";
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import BackButton from "@/components/BackButton";
 import Timer from "@/components/TimerComponent";
 import Goal from "@/components/GoalComponent";
 import PastGoals from "@/components/pastGoalComponent"
 import Colors from "@/lib/Colors";
+import { supabase } from "@/lib/Supabase";
 
 export default function FitnessPage() {
   const colorScheme = useColorScheme();
   const themeColors = colorScheme === "light" ? Colors.light : Colors.dark;
 
+  const [userId, setUserId] =  useState('');
   const [fitnessHours, setFitnessHours] = useState('');
   const [totalFitnessHours, setTotalFitnessHours] = useState(0);
   const data = [20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160];
@@ -20,6 +22,8 @@ export default function FitnessPage() {
   const [isTrackerVisible, setIsTrackerVisible] = useState(false);
   const [showSetGoal, setShowSetGoal] = useState(false);
   const [editButtonText, setEditButtonText] = useState("Edit");
+  const [fitnessId,setFitnessId] = useState('')
+
 
   const toggleSetGoal = () => {
     setShowSetGoal(!showSetGoal);
@@ -30,15 +34,85 @@ export default function FitnessPage() {
     setIsTrackerVisible(!isTrackerVisible);
   };
 
-  const handleAddFitnessHours = () => {
+  const handleAddFitnessHours = async () => {
     const inputHours = parseFloat(fitnessHours);
     const newTotalHours = totalFitnessHours + inputHours;
-    
-
+    const currentDate = new Date().toISOString().split('T')[0];
+  
     // Check if the new total is within the range of 0 to 24 hours
     if (newTotalHours >= 0 && newTotalHours <= 1440 && Number.isInteger(inputHours)) {
       setTotalFitnessHours(newTotalHours);
       setFitnessHours('');
+
+      try{
+        // checks to see if currentDate exists
+        console.log("checking to see if currentdate exists")
+        const { data, error } = await supabase
+
+        .from('fitness_details')
+        .select('fitness_id')
+        .eq('user_id', userId)
+        .gte('created_at', `${currentDate} 00:00:00`)
+        .lt('created_at', `${currentDate} 23:59:59`);
+
+        if (data !== null && data.length > 0){
+          setFitnessId(data[0].fitness_id)
+
+        }
+
+        if (data !== null && data.length > 0){
+
+          //if needs updating
+          console.log("needs updating")
+  
+          const { data, error } = await supabase
+          .from('fitness_details')
+          .update({ 'fitness_time': newTotalHours })
+          .eq('fitness_id', fitnessId);
+  
+          if (error){
+            console.log(error)
+          }
+          if (data){
+            console.log(data)
+          }
+          console.log("done")
+  
+  
+        }
+  
+        else{
+          // if needs inserting
+          console.log("needs inserting...")
+          try {
+            const { data, error } = await supabase
+  
+              .from('fitness_details')
+              .insert([
+                { user_id: userId, fitness_time: newTotalHours},
+              ])
+              .select();
+      
+            if (data) {
+              console.log(data);
+            }
+            if (error) {
+              console.log(error);
+            }
+          } catch (error) {
+            console.error('Error saving fitness details:', error);
+          }
+  
+        }
+
+        
+
+      }
+      catch(error){
+        console.log(error)
+      }
+      
+      
     } else {
       alert('Your input must be an integer, and the total fitness hours must be between 0 and 1,440 minutes (24 hours).');
     }
@@ -56,6 +130,27 @@ export default function FitnessPage() {
       alert('Your goal must be an integer between 0 and 1440 minutes (24 hours).');
     }
   }
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const { data, error } = await supabase.auth.getSession();
+  
+      if (error) {
+        console.error('Error fetching user:', error);
+        return;
+      }
+  
+      if (data && data.session && data.session.user) {
+        const user = data.session.user;
+        setUserId(user.id);
+        console.log(user.id)
+      } else {
+        setUserId(null);
+      }
+    };
+  
+    fetchUserData();
+  }, []);
 
   return (
     <ScrollView contentContainerStyle={[styles.scrollViewContent, { backgroundColor: themeColors.background }]}>
