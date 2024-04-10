dc08682c951dc783ebbf5e7a6928d0f22000fab1
Roni K <61784529+Roni003@users.noreply.github.com>
2024-04-10 17:18:19 +0100

    Changed auth page layout and font to fdm font

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,28 @@
 import { Button, StyleSheet, Pressable, Keyboard } from "react-native";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Text, View } from "@/components/Themed";
 import { Link, router } from "expo-router";
 import Auth from "@/components/Auth";
 import { supabase } from "@/lib/Supabase";
 import { redirectAfterLogin } from "@/lib/redirect";
 import { updateClientRoleId } from "@/lib/auth";
+import Colors from "@/lib/Colors";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
 export default function Entry() {
+  const [fontsLoaded, fontError] = useFonts({
+    FDMFont: require("../assets/fonts/FDMFont.ttf"),
+  });
+
+  SplashScreen.preventAutoHideAsync();
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
       redirectAfterLogin(session?.user.id);
@@ -23,13 +39,18 @@ export default function Entry() {
     });
   }, []);
 
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Pressable
         style={styles.pressableContainer}
         onPress={() => Keyboard.dismiss()}
       >
-        <Text style={styles.welcomeText}>Welcome to Fit Mind</Text>
+        <Text style={styles.fdmHeader}>FDM</Text>
+        <Text style={styles.nameHeader}>Fit Mind</Text>
         <Auth />
       </Pressable>
     </View>
@@ -40,14 +61,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 30,
-    paddingTop: 50,
+    paddingTop: "16%",
   },
   pressableContainer: {
     flex: 1,
   },
-  welcomeText: {
-    fontSize: 32,
-    fontWeight: "bold",
-    marginBottom: "20%",
+  fdmHeader: {
+    fontFamily: "FDMFont",
+    fontSize: 60,
+    alignSelf: "center",
+    paddingBottom: "4%",
+    fontWeight: "700",
+    color: Colors.dark.tint,
+  },
+  nameHeader: {
+    fontFamily: "FDMFont",
+    fontSize: 40,
+    alignSelf: "center",
+    fontWeight: "600",
+    marginBottom: "12%",
   },
 });
